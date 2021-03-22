92650d348db43e5c9e369684a252550944c74d26
Cristian Perca <cristianperca@yahoo.com>
2021-03-22 19:06:05 +0100

    Save tasks to local storage

--- src/App.js
+++ src/App.js
@@ -1,33 +1,54 @@
 import React, { Component } from "react";
 import TodayList from "./Components/TodayList";
-import CreateTaskForm from "./Components/CreateTaskForm"
+import CreateTaskForm from "./Components/CreateTaskForm";
 
 class App extends Component {
   state = {
     tasks: [
       {
         title: "Eat a banana",
-        details: "Find a banana. Eat it."
+        details: "Find a banana. Eat it.",
       },
       {
         title: "Tell The Monkey to get off his monkey butt and do something.",
-        details: ""
-      }
-  ]
-}
+        details: "",
+      },
+    ],
+  };
 
-addTask = (title, details) => {
-  let newTask = { title: title, details: details }
-  let tasks = this.state.tasks
-  tasks.push(newTask)
-  this.setState({ tasks: tasks })
-}
+  addTask = (title, details) => {
+    let newTask = { title: title, details: details };
+    let tasks = this.state.tasks;
+    tasks.push(newTask);
+    this.setState({ tasks: tasks });
+    this.updateLocalStorage();
+  };
 
+  updateLocalStorage() {
+    let tasks = JSON.stringify({
+      tasks: this.state.tasks,
+    });
+    // console.log('updateLocalStorage: this.state.tasks: ', this.state.tasks);
+    // console.log('updateLocalStorage: tasks: ', tasks);
+    
+    localStorage.setItem("tasks", tasks);
+  }
+
+  retrieveFromLocalStorage() {
+    let tasks = JSON.parse(localStorage.getItem("tasks"))
+    if (tasks) {
+      this.setState({tasks: tasks.tasks})
+    }
+  }
+  
+  componentDidMount() {
+    this.retrieveFromLocalStorage()
+  }
+  
   render() {
-
     return (
       <div className="App">
-        <CreateTaskForm addTask={this.addTask}/>
+        <CreateTaskForm addTask={this.addTask} />
         <TodayList tasks={this.state.tasks} />
       </div>
     );
